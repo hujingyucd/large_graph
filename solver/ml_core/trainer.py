b6a60a5de7cd783bd5251bd35b100a06f703d636
Richard Huan <richard20925@gmail.com>
2021-07-23 23:22:42 +0800

    Better logging

--- solver/ml_core/trainer.py
+++ solver/ml_core/trainer.py
@@ -181,8 +181,8 @@ class Trainer():
             #     probs, data.edge_index)
             self.optimizer.zero_grad()
             train_loss.backward()
-            logging.info("{}, loss {:6f}, loss_sol {:6f}".format(
-                i, train_loss.item(), loss_solution.item()))
+            logging.info("{}, loss {:6f}, score {:6f}, loss_sol {:6f}".format(
+                i, train_loss.item(), score, loss_solution.item()))
             self.optimizer.step()
         logging.info("training epoch done\n\n")
 
